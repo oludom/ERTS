9c8f3003e34eb6f1a534feb0e31b9827f2f9e47c
Micha Heiß <mhlauffen@web.de>
2021-09-13 12:44:17 +0200

    comments added

--- assignment1/ModuleSingle/main.cpp
+++ assignment1/ModuleSingle/main.cpp
@@ -5,10 +5,10 @@ using namespace sc_core;
 
 SC_MODULE (ModuleSingle) {
     sc_dt::sc_uint<4> counter;      // counter value
-    sc_event note_event;            // create event
+    sc_event e;                     // create event
     SC_CTOR(ModuleSingle){
         SC_METHOD(notifyMe);        // register method
-        sensitive << note_event;    // set sensitivity to event
+        sensitive << e;             // set sensitivity to event
         SC_THREAD(notifier);        // register thread
     }
     void notifyMe(void){
@@ -19,7 +19,7 @@ SC_MODULE (ModuleSingle) {
     void notifier(void){
         while (true){
             wait(2, SC_MS);         // delay 2ms
-            note_event.notify();    // notify method with event
+            e.notify();             // notify method with event
         }
     }
 };
