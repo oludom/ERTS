dcb86cd155e2b0f5c5ad4041f35a31c6aa99029a
Micha Heiß <mhlauffen@web.de>
2021-09-18 11:30:22 +0200

    changed defs.h to config.h

--- assignment1/exercise3.4/defs.h
+++
@@ -1,6 +0,0 @@
-#define CHANNEL_BITS	4	// Number of channel wires
-#define ERROR_BITS		2	// Number of error wires
-#define DATA_BITS		16	// Number of data wires
-#define MAX_CHANNEL		2	// Maximum number of channels actually used
-#define	CLK_PERIOD		20	// ns
-#define READY_LATENCY	1	// Number of cycles after ready that valid can be asserted after
