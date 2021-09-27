5cd6d9b6e871286f28b54b66f00e2d2f2f34e5f1
Animosa04 <deyana.atanasova04@gmail.com>
2021-09-27 09:10:21 +0200

    Added lab1

--- assignment1/exercise3.4/config.h
+++ assignment1/exercise3.4/config.h
@@ -1,16 +1,13 @@
 #ifndef CONFIG_H
-# define CONFIG_H
+#define CONFIG_H
 
-#define CHANNEL_BITS            4   // number of channel wires
-#define ERROR_BITS              2   // number of error wires
-#define DATA_BITS               16  // number of data wires
-#define MAX_CHANNEL             2   // maximum number of channels actually used
-#define CLK_PERIOD              20  // clock period in ns
-#define READY_LATENCY           1	// ready latency
+#define CHANNEL_BITS 4  // number of channel wires
+#define ERROR_BITS 2    // number of error wires
+#define DATA_BITS 16    // number of data wires
+#define MAX_CHANNEL 2   // maximum number of channels actually used
+#define CLK_PERIOD 20   // clock period in ns
+#define READY_LATENCY 1 // ready latency
 
-#define TRACE_FILE_NAME         "trace.vcd"     // name of file the trace gets saved to 
+#define TRACE_FILE_NAME "simulation_result" // name of file the trace gets saved to
 
 #endif
-
-
-
